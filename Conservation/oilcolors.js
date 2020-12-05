09cb0a2bca7acc5f539a2852262f379556fe525f
Re-Mary <Re-Mary@users.noreply.github.com>
2020-12-06 00:05:52 +0100

    Update oilcolors.js

---
+++ Conservation/oilcolors.js
@@ -0,0 +1,20 @@
+// mixed oil colors 
+function mixedOilColors () {
+    const firstOilColor = document.getElementById("firstOilColorId").value;
+    const secondOilColor = document.getElementById("secondOilColor").value;
+    if (firstOilColor, secondOilColor) {
+        
+    } else {
+        
+    }
+}
+
+function mixedOilColors () {
+    const firstOilColor = document.getElementById("firstOilColorId").value;
+    const secondOilColor = document.getElementById("secondOilColor").value;
+    if (document.getElementById("oilColors")) {
+        
+    } else {
+        
+    }
+}
