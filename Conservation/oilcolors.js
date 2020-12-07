4fc56145d689093935b747eedb340c140dd25186
Re-Mary <Re-Mary@users.noreply.github.com>
2020-12-08 00:19:39 +0100

    Update oilcolors.js

--- Conservation/oilcolors.js
+++ Conservation/oilcolors.js
@@ -1,107 +1,12 @@
-// mixed oil colors 
-
- /*   color: 'cobalt violett light';
-    color: 'cobalt violett dark';
-    color: 'ceruleum';
-    color:
-} */
-
-/*
-  
-function mixedOilColors (){
-    const firstOilColor = document.getElementById("firstOilColorId").value;
-    const secondOilColor = document.getElementById("secondOilColorId").value;
-    console.log(firstOilColor);
-    console.log(secondOilColor);
-    firstOilColor === 'cobalt blue' && secondOilColor === 'lead white' ? console.log('Will be dark'):
-    console.log ('Something is wrong')
-}  */
-
- /*   if (firstOilColor === '1' && secondOilColor === '2') {
-        console.log('It will be dark in some time')
-    } else {
-        console.log ("something is wrong")
-    }
-}; */
-
-// document.getElementById("oilColors");
-
-/*
-function mixedOilColors (){
-    const firstOilColor = document.getElementById("firstOilColorId").value;
-    const secondOilColor = document.getElementById("secondOilColorId").value;
-    let colors;
-    switch (firstOilColor && secondOilColor) {
-        case 'cobalt blue' && 'lead white':
-            console.log('Will be dark/black');
-            break;
-        case 'cobalt violet light' && 'lead white':
-            console.log('WIll be dark/black');
-            break;
-        case 'cobalt violet dark'&& 'lead white':
-            console.log('WIll be lighter');
-            break;
-        case 'cerulean blue' && 'lead white':
-            console.log('You can use it!');
-            break;
-        case 'mangan blue', 'lead white':
-            console.log('Will be lighter');
-            break;
-        case 'mangan-cadmium', 'lead white':
-            console.log('You can use it!');
-            break;
-        case 'cadmium yellow', 'lead white':
-            console.log('You can use it!');
-            break;     
-        case 'cadmium orange', 'lead white':
-            console.log('You can use it!');
-            break;
-        case 'cadmium red', 'lead white':
-            console.log('You can use it!');
-            break;
-        case 'transparent mars yellow', 'lead white':
-            console.log('You can use it!');
-            break;
-        case 'transparent mars brown deep', 'lead white':
-            console.log('Will be lighter');
-            break;
-        case 'english red', 'lead white':
-            console.log('You can use it!');
-            break;
-        case 'caput-mortuum', 'lead white':
-            console.log('WIll be dark/black');
-            break;
-        case 'ochre', 'ochre light', 'gold ochre', 'red ochre', 'lead white':
-            console.log('You can use it!');
-            break;
-        case 'ochre deep', 'ochre deep kotayk', 'lead white':
-            console.log('Will be lighter');
-            break;
-        case 'raw sienna', 'burnt sienna', 'lead white':
-            console.log('You can use it!');
-            break;
-        case 'mars brown deep', 'lead white':
-            console.log('You can use it!');
-            break;
-        case 'mars brown light', 'lead white':
-            console.log('Will be lighter');
-            break;
-                               
-    }
-
-// another variante
-    } else if (firstOilColor === 'cobalt violet dark' && secondOilColor === 'lead white'){
-        console.log('Will be lighter');
-    } else if (firstOilColor === 'cerulean blue' && secondOilColor === 'lead white'){
-        console.log('You can use it!');
-
-} */
+// Mix of oil colors
 
 let Oilcolor = { 
     red: ["english red", "cadmium red" ],
-    green: "glauconite",
-    blue: "cobalt blue",
-    black: "It will be black"
+    crack: "Your paint layer could cracking",
+    colorChangeCrack: "Your paint layer could change the color and cracking",
+    couldBeUsed: "You can use it!",
+    aufhellung: "Change the color, lightening",
+    black: "Change the color into black, darkening"
 }
 console.log(Oilcolor.red);
 
@@ -110,18 +15,96 @@ function mixedOilColors (){
     const secondOilColor = document.getElementById("secondOilColorId").value;
     
     if (firstOilColor==='cobalt blue' && secondOilColor==='lead white' ||
-        firstOilColor === 'lead white' && secondOilColor === "cobalt white" || ) {
+        firstOilColor === 'lead white' && secondOilColor === 'cobalt blue' ||
+        firstOilColor === 'cobalt violet light' && secondOilColor === 'lead white' ||
+        firstOilColor === 'lead white' && secondOilColor === 'cobalt violet light' ||             
+        firstOilColor === 'caput-mortuum' && secondOilColor === 'lead white' ||
+        firstOilColor === 'lead white' && secondOilColor === 'caput-mortuum' ||
+        firstOilColor === 'madder lake red permanent' && secondOilColor === 'lead white' ||
+        firstOilColor === 'lead white' && secondOilColor === 'madder lake red permanent' ||
+        firstOilColor === 'gold ochre' && secondOilColor === 'lead white' ||
+        firstOilColor === 'lead white' && secondOilColor === 'gold ochre' ||
+        firstOilColor === 'ultramarine blue' && secondOilColor === 'lead white' ||
+        firstOilColor === 'lead white' && secondOilColor === 'ultramarine blue'        
+        ) {
         console.log('Will be black');
-        document.querySelector("#res2").textContent = Oilcolor.red[1];
-    } else if (firstOilColor==='cobalt violet light' && secondOilColor==='lead white') {
-        console.log('WIll be dark/black');
-    } else if (firstOilColor==='cobalt violet dark' && secondOilColor==='lead white') {
+        document.querySelector("#res2").textContent = Oilcolor.black;
+
+    } else if (firstOilColor==='cobalt violet dark' && secondOilColor==='lead white' ||
+            firstOilColor==='lead white' && secondOilColor==='cobalt violet dark' ||
+            firstOilColor === 'mangan blue' && secondOilColor === 'lead white' ||
+            firstOilColor === 'lead white' && secondOilColor === 'mangan blue' ||
+            firstOilColor === 'transparent mars brown deep' && secondOilColor === 'lead white' ||
+            firstOilColor === 'lead white' && secondOilColor === 'transparent mars brown deep' ||
+            firstOilColor === 'ochre deep' && secondOilColor === 'lead white' ||
+            firstOilColor === 'lead white' && secondOilColor === 'ochre deep' ||
+            firstOilColor === 'mars brown light' && secondOilColor === 'lead white' ||
+            firstOilColor === 'lead white' && secondOilColor === 'mars brown light' ||
+            firstOilColor === 'raw umber' && secondOilColor === 'lead white' ||
+            firstOilColor === 'lead white' && secondOilColor === 'raw umber' ||
+            firstOilColor === 'madder lake rose permanent' && secondOilColor === 'lead white' ||
+            firstOilColor === 'lead white' && secondOilColor === 'madder lake rose permanent' ||
+            firstOilColor === 'madder lake purple permanent' && secondOilColor === 'lead white' ||
+            firstOilColor === 'lead white' && secondOilColor === 'madder lake purple permanent' ||   
+            firstOilColor === 'madder lake purple permanent' && secondOilColor === 'lead white' ||
+            firstOilColor === 'lead white' && secondOilColor === 'madder lake purple permanent' ||
+            
+            firstOilColor === 'madder lake purple permanent' && secondOilColor === 'zinc white' ||
+            firstOilColor === 'zinc white' && secondOilColor === 'madder lake purple permanent' ||
+
+            firstOilColor === 'madder lake purple permanent' && secondOilColor === 'strontium yellow' ||
+            firstOilColor === 'strontium yellow' && secondOilColor === 'madder lake purple permanent' || 
+
+            firstOilColor === 'madder lake purple permanent' && secondOilColor === 'smaragd green' ||
+            firstOilColor === 'smaragd green' && secondOilColor === 'madder lake purple permanent' ||            
+            firstOilColor === 'madder lake red permanent' && secondOilColor === 'smaragd green' ||
+            firstOilColor === 'smaragd green' && secondOilColor === 'madder lake red permanent' ||            
+            firstOilColor === 'madder lake rose permanent' && secondOilColor === 'smaragd green' ||
+            firstOilColor === 'smaragd green' && secondOilColor === 'madder lake rose permanent' ||
+
+            firstOilColor === 'madder lake purple permanent' && secondOilColor === 'cadmium lemon' ||
+            firstOilColor === 'cadmium lemon' && secondOilColor === 'madder lake purple permanent' ||  
+            firstOilColor === 'madder lake purple permanent' && secondOilColor === 'cadmium yellow' ||
+            firstOilColor === 'cadmium yellow' && secondOilColor === 'madder lake purple permanent' ||  
+            firstOilColor === 'madder lake purple permanent' && secondOilColor === 'cadmium orange' ||
+            firstOilColor === 'cadmium orange' && secondOilColor === 'madder lake purple permanent' ||  
+            firstOilColor === 'madder lake purple permanent' && secondOilColor === 'cadmium red' ||
+            firstOilColor === 'cadmium red' && secondOilColor === 'madder lake purple permanent' ||
+            
+            firstOilColor === 'madder lake purple permanent' && secondOilColor === 'ultramarine blue' ||
+            firstOilColor === 'ultramarine blue' && secondOilColor === 'madder lake purple permanent' ||             
+
+            firstOilColor === 'madder lake purple permanent' && secondOilColor === 'madder lake rose permanent' ||
+            firstOilColor === 'madder lake rose permanent' && secondOilColor === 'madder lake purple permanent' ||
+            firstOilColor === 'madder lake purple permanent' && secondOilColor === 'madder lake red permanent' ||
+            firstOilColor === 'madder lake red permanent' && secondOilColor === 'madder lake purple permanent'
+    ) {
         console.log('Will be lighter');
-    } else if (firstOilColor==='cerulean blue' && secondOilColor==='lead white') {
+        document.querySelector("#res2").textContent = Oilcolor.aufhellung;
+
+    } else if (firstOilColor === 'mangan blue' && secondOilColor === 'green earth' ||
+            firstOilColor === 'green earth' && secondOilColor === 'mangan blue' ||
+            firstOilColor === 'raw umber' && secondOilColor === 'green earth' ||
+            firstOilColor === 'green earth' && secondOilColor === 'raw umber' ||     
+            firstOilColor === 'madder lake red permanent' && secondOilColor === 'green earth' ||
+            firstOilColor === 'green earth' && secondOilColor === 'madder lake red permanent' ||
+            firstOilColor === 'madder lake rose permanent' && secondOilColor === 'green earth' ||
+            firstOilColor === 'green earth' && secondOilColor === 'madder lake rose permanent' ||
+            firstOilColor === 'madder lake purple permanent' && secondOilColor === 'green earth' ||
+            firstOilColor === 'green earth' && secondOilColor === 'madder lake purple permanent' ||
+            firstOilColor === 'gold yellow' && secondOilColor === 'green earth' ||
+            firstOilColor === 'green earth' && secondOilColor === 'gold yellow' ||
+            firstOilColor === 'peach black' && secondOilColor === 'green earth' ||
+            firstOilColor === 'green earth' && secondOilColor === 'peach black' ||   
+            firstOilColor === 'blue black' && secondOilColor === 'green earth' ||
+            firstOilColor === 'green earth' && secondOilColor === 'blue black' ||              
+    ) {
         console.log('You can use it!');
+        document.querySelector("#res2").textContent = Oilcolor.crack;
+
     } else {
-        console.log ('Please, fill in the fields')
- //       alert('Please, fill in the fields')
+        console.log ('No data avaliable')
+
     }
 
 };
